1bb05a82fccb531374a049dc58fbdb461b328c36
winpzs <winpzs@gmail.com>
2017-06-28 14:02:12 +0800

    cmpx-loader 1.0.0-deta3

--- loader/index.js
+++ loader/index.js
@@ -1,7 +1,5 @@
 
-var path = require('path'),
-    fs = require('fs'),
-    build = require('cmpx-build');
+var build = require('cmpx-build');
 
 module.exports = function(source){
   let {content, files} = build.buildTypeScript(this.resourcePath, source);
